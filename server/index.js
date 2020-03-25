08db5a7b2bae3a3188071bcf8d335c25e5b05391
Proger3228 <damir221003@gmail.com>
2020-03-25 14:56:08 +0400

    initial commmit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const { ApolloServer, gql } = require('apollo-server-express');
+// const { typeDefs, resolvers } = require('./schema');
+
+// const server = new ApolloServer({
+//     // typeDefs,
+//     // resolvers,
+// });
+//
+// server.applyMiddleware({ app });
+//
+// app.listen({ port: 4000 }, () =>
+//     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+// )
