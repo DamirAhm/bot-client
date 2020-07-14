e4837d666420e0eb78b1f44c3dfffbd6f11b0b4d
Proger3228 <damir221003@gmail.com>
2020-07-14 20:53:48 +0400

    Fixed sidebar styles

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -6,17 +6,14 @@ import { FaChevronLeft } from "react-icons/fa";
 const Sidebar: React.FC = () => {
     return (
         <div onMouseDown={e => e.stopPropagation()} className={styles.sidebar}>
+            <input type="checkbox" id={styles.check} />
             <label htmlFor={styles.check}>
-                {document.querySelector(`#${styles.check}:checked`) ?
-                    <FaChevronLeft size={20} /> :
-                    <GiHamburgerMenu size={20} className={styles.hamb} />
-                }
+                <FaChevronLeft className={styles.opened} size={20} />
+                <GiHamburgerMenu className={styles.closed} size={20}/>
             </label>
-            <input onChange={() => document.querySelector(`.wrapper`)?.classList.toggle("sidebarOpened")} type="checkbox" id={styles.check} />
             <NavLink to="/classes" className={styles.link} activeClassName={styles.active}> Классы </NavLink>
             <NavLink to="/students" className={styles.link} activeClassName={styles.active}> Ученики </NavLink>
         </div>
     )
 };
-
 export default Sidebar;
