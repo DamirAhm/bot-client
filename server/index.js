14c9cf8d7db263a7174c7063784fd72e4442fa38
DamirAhm <damir221003@yandex.ru>
2020-08-29 01:26:56 +0400

    Returned serving and added compression module

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
-const app = require("express")();
-const { ApolloServer } = require("apollo-server-express");
-const mongoose = require("mongoose");
-const { graphqlSchema } = require("./schema");
-const VK_API = require("bot-database/VkAPI/VK_API");
-const config = require("./config.json");
-const cors = require("cors");
-const multer = require("multer");
-const fs = require("fs");
-const { DataBase: DB } = require("bot-database/DataBase");
-const path = require("path");
+const app = require( "express" )();
+const { ApolloServer } = require( "apollo-server-express" );
+const { graphqlSchema } = require( "./schema" );
+const { VK_API } = require( "bot-database" );
+const config = require( "./config.json" );
+const cors = require( "cors" );
+const multer = require( "multer" );
+const fs = require( "fs" );
+const { DataBase: DB } = require( "bot-database/DataBase" );
+const path = require( "path" );
+const sirv = require( "sirv" );
+const compression = require( "compression" );
 
-const DataBase = new DB(config["MONGODB_URI"]);
+const DataBase = new DB( config[ "MONGODB_URI" ] );
 const vk = new VK_API(
-    config["VK_API_KEY"],
-    config["GROUP_ID"],
-    config["ALBUM_ID"]
+    config[ "VK_API_KEY" ],
+    config[ "GROUP_ID" ],
+    config[ "ALBUM_ID" ]
 );
 
-const getFileExtension = (fileName) => fileName.match(/(.*)\.[^.]+$/)[0];
+const getFileExtension = ( fileName ) => fileName.match( /(.*)\.[^.]+$/ )[ 0 ];
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
+const storage = multer.diskStorage( {
+    destination: function ( req, file, cb ) {
+        cb( null, path.join( __dirname, "uploads" ) );
     },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    filename: function ( req, file, cb ) {
+        const uniqueSuffix = Date.now() + "-" + Math.round( Math.random() * 1e9 );
         cb(
             null,
             file.fieldname +
-                "-" +
-                uniqueSuffix +
-                getFileExtension(file.originalname)
+            "-" +
+            uniqueSuffix +
+            getFileExtension( file.originalname )
         );
     },
-});
+} );
 
-var upload = multer({ storage: storage });
+var upload = multer( { storage: storage } );
 
 DataBase.connect(
     {
@@ -43,10 +44,10 @@ DataBase.connect(
         useUnifiedTopology: true,
         useCreateIndex: true,
     },
-    () => console.log("Mongoose connected")
+    () => console.log( "Mongoose connected" )
 );
 
-const server = new ApolloServer({
+const server = new ApolloServer( {
     typeDefs: `
         attachment {
             value: String
@@ -59,46 +60,42 @@ const server = new ApolloServer({
         }
     `,
     schema: graphqlSchema,
-});
+} );
 
-server.applyMiddleware({ app });
+server.applyMiddleware( { app } );
 
-app.use(cors());
+app.use( cors() );
+app.use( sirv( "build" ) );
+app.use( compression() );
 
-app.post("/saveAttachment", upload.array("newAttachment"), async (req, res) => {
+app.post( "/saveAttachment", upload.array( "newAttachment" ), async ( req, res ) => {
     try {
         const photos = [];
-
-        for (const file of req.files) {
-            const readStream = fs.createReadStream(file.path);
-            const photo = await vk.uploadPhotoToAlbum(readStream);
-            photos.push(photo[0]);
-            fs.unlink(file.path, function (err) {
-                if (err) {
-                    console.error(err);
+        for ( const file of req.files ) {
+            const readStream = fs.createReadStream( file.path );
+            const photo = await vk.uploadPhotoToAlbum( readStream );
+            photos.push( photo[ 0 ] );
+            fs.unlink( file.path, function ( err ) {
+                if ( err ) {
+                    console.error( err );
                 }
-            });
+            } );
         }
 
-        res.json({ photos });
-    } catch (e) {
-        console.error(e);
-        res.send({ error: e });
+        res.json( { photos } );
+    } catch ( e ) {
+        console.error( e );
+        res.send( { error: e.message, stack: e.stack } );
     }
-});
+} );
 
-app.get("/*", (req, res) => {
-    if (/\.(js|css|html|svg|ico|png|jpg|webp)$/i.test(req.url)) {
-        res.sendFile(path.join(__dirname, "build", req.url));
-    } else {
-        res.sendFile(__dirname + "/build/index.html");
-    }
-});
+app.get( "/*", ( req, res ) => {
+    res.sendFile( __dirname + "/build/index.html" );
+} );
 
-app.listen({ port: process.env.PORT || 8080 }, () =>
+app.listen( { port: process.env.PORT || 8080 }, () =>
     console.log(
-        `🚀 Server ready at http://localhost:${process.env.PORT || 8080}${
-            server.graphqlPath
+        `🚀 Server ready at http://localhost:${process.env.PORT || 8080}${server.graphqlPath
         }`
     )
 );
