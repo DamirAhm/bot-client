c2cbc22c8f55127a6d6fd0e3713cebca408e3943
DamirAhm <damir221003@yandex.ru>
2020-09-17 22:58:58 +0400

    Updated bot-database package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,118 +1,109 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import ApolloClient, { gql } from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { BrowserRouter } from "react-router-dom";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { GET_CLASSES } from "./components/Content/Classes/Classes";
-import { Class } from "./types";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter } from 'react-router-dom';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GET_CLASSES } from './components/Content/Classes/Classes';
+import { Class } from './types';
 
 const resolvers = {
-    Mutation: {
-        deleteClass: (
-            _: any,
-            { name }: { name: string },
-            { cache }: { cache: InMemoryCache }
-        ) => {
-            const classes = cache.readQuery<{ classes: Class[] }>({
-                query: GET_CLASSES
-            });
+	Mutation: {
+		deleteClass: (_: any, { name }: { name: string }, { cache }: { cache: InMemoryCache }) => {
+			const classes = cache.readQuery<{ classes: Class[] }>({
+				query: GET_CLASSES,
+			});
 
-            const newClasses =
-                classes?.classes?.filter((c: Class) => c.name !== name) || [];
+			const newClasses = classes?.classes?.filter((c: Class) => c.name !== name) || [];
 
-            cache.writeQuery({
-                query: GET_CLASSES,
-                data: {
-                    classes: newClasses
-                }
-            });
+			cache.writeQuery({
+				query: GET_CLASSES,
+				data: {
+					classes: newClasses,
+				},
+			});
 
-            return newClasses;
-        },
-        createClass: (
-            _: any,
-            { name }: { name: string },
-            { cache }: { cache: InMemoryCache }
-        ) => {
-            const data = cache.readQuery<{
-                classes: { name: string; studentsCount: number }[];
-            }>({ query: GET_CLASSES });
+			return newClasses;
+		},
+		createClass: (_: any, { name }: { name: string }, { cache }: { cache: InMemoryCache }) => {
+			const data = cache.readQuery<{
+				classes: { name: string; studentsCount: number }[];
+			}>({ query: GET_CLASSES });
 
-            if (data !== null) {
-                const newClasses = [
-                    ...data.classes,
-                    {
-                        name,
-                        studentsCount: 0,
-                        __typename: "Class",
-                        _id: Date.now().toString()
-                    }
-                ];
-                cache.writeQuery({
-                    query: GET_CLASSES,
-                    data: {
-                        classes: newClasses
-                    }
-                });
+			if (data !== null) {
+				const newClasses = [
+					...data.classes,
+					{
+						name,
+						studentsCount: 0,
+						__typename: 'Class',
+						_id: Date.now().toString(),
+					},
+				];
+				cache.writeQuery({
+					query: GET_CLASSES,
+					data: {
+						classes: newClasses,
+					},
+				});
 
-                return {
-                    name,
-                    studentsCount: 0,
-                    __typename: "Class",
-                    _id: Date.now().toString()
-                };
-            }
-            return null;
-        }
-    }
+				return {
+					name,
+					studentsCount: 0,
+					__typename: 'Class',
+					_id: Date.now().toString(),
+				};
+			}
+			return null;
+		},
+	},
 };
 
 const typeDefs = gql`
-    fragment StudentPreview on Student {
-        vkId
-        className
-        role
-        fullName
-        _id
-    }
-    fragment ClassPreview on Class {
-        name
-        studentsCount
-    }
-    extend type Mutation {
-        deleteClass(name: String): [Class]!
-        createClass(name: String): Class!
-    }
+	fragment StudentPreview on Student {
+		vkId
+		className
+		role
+		fullName
+		_id
+	}
+	fragment ClassPreview on Class {
+		name
+		studentsCount
+	}
+	extend type Mutation {
+		deleteClass(name: String): [Class]!
+		createClass(name: String): Class!
+	}
 `;
 
 const client = new ApolloClient({
-    uri: process.env.NODE_ENV === "development" ? "http://localhost:8080/graphql" : "/graphql",
-    cache: new InMemoryCache({
-        dataIdFromObject: (obj: any) => {
-            if (obj._id) {
-                return obj._id;
-            } else {
-                return null;
-            }
-        }
-    }),
-    resolvers,
-    typeDefs,
+	uri: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/graphql' : '/graphql',
+	cache: new InMemoryCache({
+		dataIdFromObject: (obj: any) => {
+			if (obj._id) {
+				return obj._id;
+			} else {
+				return null;
+			}
+		},
+	}),
+	resolvers,
+	typeDefs,
 });
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <BrowserRouter>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
-        </BrowserRouter>
-    </ApolloProvider>,
-    document.getElementById("root")
+	<ApolloProvider client={client}>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</ApolloProvider>,
+	document.getElementById('root'),
 );
 
-serviceWorker.unregister();
+serviceWorker.register();
