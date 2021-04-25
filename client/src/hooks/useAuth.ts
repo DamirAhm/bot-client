a2398b4a295fc02953cfabd8484bc615057309f5
DamirAhm <damir221003@gmail.com>
2021-04-25 13:16:21 +0400

    Fixed authentification (local storage cleaned if it has user in it)

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,8 +1,8 @@
-import { useApolloClient } from '@apollo/client';
-import md5 from 'md5';
-import { useEffect, useState } from 'react';
-import { GET_STUDENT_BY_VK_ID } from '../components/Content/StudentPage/StudentPage';
-import { User, Student, setStateProp, roles } from '../types';
+import { useApolloClient } from "@apollo/client";
+import md5 from "md5";
+import { useEffect, useState } from "react";
+import { GET_STUDENT_BY_VK_ID } from "../components/Content/StudentPage/StudentPage";
+import { User, Student, setStateProp, roles } from "../types";
 
 const useAuth = () => {
 	const ApolloClient = useApolloClient();
@@ -10,7 +10,7 @@ const useAuth = () => {
 	const [user, setUser] = useState<User | null>(null);
 
 	const getFullUser = async (
-		vkUser: Omit<returnUserType, 'hash' | 'session'>,
+		vkUser: Omit<returnUserType, "hash" | "session">
 	): Promise<User | null> => {
 		const {
 			data: { student },
@@ -33,8 +33,8 @@ const useAuth = () => {
 
 		if (fullUser) {
 			setUser(fullUser);
-			localStorage.setItem('user', JSON.stringify(user));
-			localStorage.setItem('hash', hash);
+			localStorage.setItem("user", JSON.stringify(user));
+			localStorage.setItem("hash", hash);
 		} else {
 			cleanLocalStorage();
 		}
@@ -60,18 +60,18 @@ const useAuth = () => {
 export default useAuth;
 
 function getUserFromStorage() {
-	const userItem = localStorage.getItem('user');
-	const hash = localStorage.getItem('hash');
+	const userItem = localStorage.getItem("user");
+	const hash = localStorage.getItem("hash");
 
 	const app_id = process.env.REACT_APP_APP_ID;
 	const secret = process.env.REACT_APP_SECRET;
 
 	if (userItem && hash) {
 		const parsedUser = JSON.parse(userItem);
-		if (typeof parsedUser === 'object') {
+		if (typeof parsedUser === "object") {
 			if (
-				['first_name', 'last_name', 'uid', 'photo_rec'].every((key) =>
-					Object.keys(parsedUser).includes(key),
+				["first_name", "last_name", "uid", "photo_rec"].every(key =>
+					Object.keys(parsedUser).includes(key)
 				)
 			) {
 				if (hash === md5(app_id + parsedUser.uid + secret)) {
@@ -79,14 +79,14 @@ function getUserFromStorage() {
 				}
 			}
 		}
+	} else {
+		cleanLocalStorage();
 	}
-
-	cleanLocalStorage();
 	return null;
 }
 
 function cleanLocalStorage() {
-	console.log('CLEAN');
-	localStorage.removeItem('user');
-	localStorage.removeItem('hash');
+	console.log("CLEAN");
+	localStorage.removeItem("user");
+	localStorage.removeItem("hash");
 }
